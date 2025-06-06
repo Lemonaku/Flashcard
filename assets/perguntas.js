8c27da7f8bf0e8a45928e92dc1bb4b13b36b7613
Lemonaku <luana.goncalves11@escola.pr.gov.br>
2025-06-06 18:48:32 -0300

    a

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,19 +1,19 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Biologia',
+    'O que é a biologia?',
+    'Biologia é a ciência que estuda a vida, como ela se relaciona entre os seres, como se mantém.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Biologia',
+    'O que é um mamífero?',
+    'São animais vertebrados que se caracterizam pela presença de pelos no corpo, e também, glândulas mamárias que produzem leite para alimentar os filhotes.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Biologia',
+    'Qual a unidade fundamental dos seres vivos?',
+    'As células, elas formam os seres vivos em sua constituição, onde são formados por trilhões ou até, apenas uma.'
 )
 
 criaCartao(
